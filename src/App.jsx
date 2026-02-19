c65fdc7e7473aeb3197cc8b77c9c7bcc58ad29ab
Vignesh-Thamizhan <v6074402@gmail.com>
2026-02-19 12:03:09 +0530

    terminal made

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import { Dock, Navbar, Welcome, Windows } from './components'
+import gsap from 'gsap';
+gsap.registerPlugin(Draggable);
+
+import { Draggable } from 'gsap/all';
+import { Dock, Navbar, Welcome, Windows } from './components';
+import { Terminal } from './windows';
+
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <Welcome />
-      
       <Dock />
+
+      <Terminal />
     </main>
   )
 }
