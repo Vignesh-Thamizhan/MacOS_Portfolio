3cfda3ada729f26dc6ab27231ec1b0a32581ada0
Vignesh-Thamizhan <v6074402@gmail.com>
2026-02-20 12:02:57 +0530

    beginning of window controls

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import { Navbar } from "./Navbar";
 import { Welcome } from "./Welcome";
 import {Dock} from "./Dock";
 import { Windows } from "./Windows";
+import WindowControls from "./WindowControls";
 
 export {
   Navbar,
   Welcome,
   Dock,
   Windows,
+  WindowControls,
 };
